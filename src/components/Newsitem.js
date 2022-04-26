6ea561671083f631dfdd7988ede023480ad90901
bhavin1998 <bhavingediya123@gmail.com>
2022-04-26 17:39:59 +0530

    Display news from JSON

---
+++ src/components/Newsitem.js
@@ -0,0 +1,24 @@
+import PropTypes from 'prop-types'
+import React, { Component } from 'react'
+
+export class Newsitem extends Component {
+  static propTypes = {}
+
+  render() {
+      let {title,postdesc,imageurl,newsurl} = this.props
+    return (
+    <div>
+        <div className="card" style={{width: "18rem"}}>
+            <img src={imageurl} className="card-img-top" alt="..."/>
+            <div className="card-body">
+            <h5 className="card-title">{title}...</h5>
+            <p className="card-text">{postdesc}...</p>
+            <a href={newsurl} className="btn btn-primary btn-sm" target="_blank">Read More</a>
+            </div>
+        </div>
+    </div>
+    )
+  }
+}
+
+export default Newsitem
